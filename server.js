d2124dcb09ef55d5b7d047fa41eef2ceb385b95b
mydatascience1608 <mydatascience1608@gmail.com>
2025-06-03 22:59:27 +0700

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'Public')));
 // Middleware
 app.use(cors());
 app.use(express.json());
