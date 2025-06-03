c4fcfe2f8ab8b0c282ea72636c04b1afd73ed078
mydatascience1608 <mydatascience1608@gmail.com>
2025-06-03 23:03:53 +0700

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'Public')));
 // Middleware
